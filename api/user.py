9f062c8b088ad9075f7db6ce3091d7b3931ffdf8
Pop Ionut Remus <codercoder42@gmail.com>
2023-02-27 00:12:19 +0200

    Update user.py

--- api/user.py
+++ api/user.py
@@ -5,5 +5,5 @@ from pymongo import MongoClient
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        print('TEST 1234567')
+        print('TEST ssssssssssssssssssssssssssssssssssssssssssssssssssssss')
         return
