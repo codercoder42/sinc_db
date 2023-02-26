5f71b8a4f39d6ba028a45aff0dc059112055e113
Pop Ionut Remus <codercoder42@gmail.com>
2023-02-26 23:20:57 +0200

    Update user.py

--- api/user.py
+++ api/user.py
@@ -1,4 +1,5 @@
 from http.server import BaseHTTPRequestHandler
+from os.path import join
 
 class handler(BaseHTTPRequestHandler):
 
@@ -6,5 +7,7 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
+        with open(join('data', 'file.txt'), 'r') as file:
+          for line in file:
+            self.wfile.write(line.encode())
         return
